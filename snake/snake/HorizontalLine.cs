0cb29faffadd8a1dc5ff2bb98965a441fa2d12ec
Demnart <insidia93@gmail.com>
2016-03-29 13:37:39 +0300

    создание кода горизонтальной линии

---
+++ snake/snake/HorizontalLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<Point> lpoint;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+
+            lpoint = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                lpoint.Add(p);
+            }
+        
+
+        }
+
+            public void Draw() {
+
+            foreach (Point p in lpoint) {
+                p.Drow();
+            }
+        }
+
+
+        }
+    }
+
