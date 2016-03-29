5f49f5e96c2347d6cda37d492714a9934c9042f2
Demnart <insidia93@gmail.com>
2016-03-29 16:45:42 +0300

    Создание класса змейки и реализация конструктора  который на вход принимает точку конца хвоста, длину змейки и ее направление.

---
+++ snake/snake/Snake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point tail, int lengh, Direction direction) {
+            lpoint = new List<Point>();
+            for (int i = 0; i < lengh; i++) {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                lpoint.Add(p);
+
+            }
+        }
+    }
+}
