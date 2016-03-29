a4fdd11dc4bda865e59b1a65ed3265674b0cd000
Demnart <insidia93@gmail.com>
2016-03-29 16:43:30 +0300

    ДОбавление копирующего конструктора. Создание функции Move определяющей направление курсора.

--- snake/snake/point.cs
+++ snake/snake/point.cs
@@ -20,10 +20,46 @@ namespace snake
             this.simb = simb;
         }
 
+        public Point(Point p) {
+            x = p.x;
+            y = p.y;
+            simb = p.simb;
+
+
+        }
+
+        public void Move(int offset, Direction direction) {
+
+            if (direction == Direction.Right)
+            {
+                x = x + offset;
+            }
+
+            else if (direction == Direction.Left)
+            {
+                x = x - offset;
+
+            }
+
+            else if (direction == Direction.Up)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.Down)
+            {
+                y = y - offset;
+            }
+        }
+
+
         public  void Drow()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(simb);
+        }
+        public override string ToString()
+        {
+            return x + "," + y + "," + simb;
         }
     }
 }
