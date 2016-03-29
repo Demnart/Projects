9c95deb835c3ad55c12dcb33e579c2c73c68b379
Demnart <insidia93@gmail.com>
2016-03-29 13:37:53 +0300

    создание кода вертикальной линии

---
+++ snake/snake/VerticalLine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class VerticalLine
+    {
+        List<Point> lpoint;
+
+        public VerticalLine(int x, int yHight, int yBoth, char sym) {
+
+            lpoint = new List<Point>();
+            for (int y = yHight; y <= yBoth; y++)
+            {
+                Point p = new Point(x, y, sym);
+                lpoint.Add(p);
+            }
+
+
+        }
+        
+        public void Draw()
+        {
+
+            foreach (Point p in lpoint)
+            {
+                p.Drow();
+            }
+        }
+
+
+    }
+}
+
